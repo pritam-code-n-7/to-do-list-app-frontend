436f99e1a962114ba8af6092b74a9e1348360593
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-03-22 12:07:49 +0530

    modified app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
 import Dashboard from "./components/Dashboard";
 
-
 function App() {
   return (
-    // <div>   
-    //   <Body />
-    // </div>
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Body />} />
-    <Route path="/dashboard" element={<Dashboard/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
